0c9af0b3873d7277d9e0272d77b7570afad6e4c1
이경민 <@DESKTOP-B8C9N61>
2020-01-27 20:25:00 +0900

    2.2.1장 테스트 검증의 자동화

--- study/src/main/java/springbook/user/test/UserDaotest.java
+++ study/src/main/java/springbook/user/test/UserDaotest.java
@@ -16,6 +16,11 @@ import springbook.user.domain.User;
  *  - 1.3.3 관계설정 책임의 분리 : UserDaotest는 UserDao와 ConnectionMaker 구현 클래스와의 런타임 오브젝트 의존관계를 설정하는 책임을 담당 -> UserDao에 있으면 안되는 다른 관심사항, 책임을 떠넘기는 작업
  *  - 1.5.1 오브젝트 팩토리를 이용한 스프링 IoC
  *  - 1.8.2 XML을 이용하는 애플리케이션 컨텍스트
+ * 2장 테스트
+ *  - 2.1.3 UserDaoTest의 문제점 두가지
+ * 		(1) 수동 확인 작업의 번거로움
+ * 		(2) 실행 작업의 번거로움
+ *  - 2.2.1 테스트 검증의 자동화
  */
 public class UserDaotest {
 	
@@ -37,10 +42,14 @@ public class UserDaotest {
 		System.out.println(user.getId() + " 등록 성공");
 		
 		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
 		
-		System.out.println(user2.getId() + " 조회 성공");
+		if(!user.getName().equals(user2.getName())) {
+			System.out.println("테스트 실패 (name)");
+		}else if (!user.getPassword().equals(user2.getPassword())) {
+			System.out.println("테스트 실패 (password)");
+		}else {
+			System.out.println("조회 테스트 성공");
+		}
 	}
 	
 }
