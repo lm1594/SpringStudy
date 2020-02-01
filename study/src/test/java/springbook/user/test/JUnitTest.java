6fd5fdc353238273ae8159af8d88e5b8fabd868a
이경민 <@DESKTOP-B8C9N61>
2020-02-01 15:14:16 +0900

    2.5.2 학습테스트 예제

---
+++ study/src/test/java/springbook/user/test/JUnitTest.java
@@ -0,0 +1,71 @@
+package springbook.user.test;
+
+import static org.junit.matchers.JUnitMatchers.hasItem;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.matchers.JUnitMatchers.either;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * 토비의 스프링
+ * @author 이경민
+ * @since 2020.02.01
+ * @history
+ * 2.5장 학습테스트로 배우는 스프링
+ *  - 2.5.2 학습테스트 예제 
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "junit.xml")
+public class JUnitTest {
+
+	@Autowired
+	ApplicationContext context;
+	
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+	static ApplicationContext contextObject = null;
+	
+	@Test
+	public void test1() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		
+		assertThat(contextObject == null || contextObject == this.context, is(true));
+		contextObject = this.context;
+	}
+	
+	@Test
+	public void test2() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		
+		assertTrue(contextObject == null || contextObject == this.context);
+		contextObject = this.context;
+	}
+	
+	@Test
+	public void test3() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		
+		assertThat(contextObject, either(is(nullValue())).or(is(this.context)));
+		contextObject = this.context;
+	}
+	
+	public static void main(String[] args) {
+		JUnitCore.main("springbook.user.test.JUnitTest");
+	}
+}
