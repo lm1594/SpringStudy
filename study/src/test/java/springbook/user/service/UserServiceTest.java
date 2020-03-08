3ce15b2c3e3ee5babf7a7f10ebf9059856be74f9
이경민 <@DESKTOP-B8C9N61>
2020-03-08 13:07:17 +0900

    5.4.4테스트대역 / 5장 정리

--- study/src/test/java/springbook/user/service/UserServiceTest.java
+++ study/src/test/java/springbook/user/service/UserServiceTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,7 +15,9 @@ import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -43,6 +46,7 @@ import static springbook.user.service.UserService.MIN_RECCOMEND_FOR_GOLD;
  *  5장 서비스 추상화
  *   5.4장 메일서비스 추상화
  *    - 5.4.3 테스트를 위한 서비스 추상화 : 테스트용 메일발송 오브젝트
+ *    - 5.4.4 테스트 대역 : 목 오브젝트를 이용한 테스트
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/applicationContext.xml")
@@ -67,7 +71,9 @@ public class UserServiceTest {
 	}
 	
 	/**
-	 * 5.1.3 사용자레벨 업그레이드 테스트
+	 * @history
+	 *  - 5.1.3 사용자레벨 업그레이드 테스트
+	 *  - 리스트 5-57 목 오브젝트로 만든 메일 전송 확인용 클래스
 	 */
 	@Test
 	public void upgradeLevels() throws Exception{
@@ -76,6 +82,11 @@ public class UserServiceTest {
 			userDao.add(user);
 		}
 		
+		// 메일 발송 결과를 테스트할 수 있도록 목 오브젝트를 만들어 UserSerivce의 의존 오브젝트로 주입해준다.
+		MockMailSender mockMailSender = new MockMailSender();
+		userService.setMailSender(mockMailSender);
+		
+		// 업그레이드 테스트, 메일 발송이 일어나면 MockMailSender오브젝트의 리스트에 그 결과가 저장된다.
 		userService.upgradeLevels();
 		
 		// 각 사용자별로 업그레이드 후의 예상 레벨을 검증한다.
@@ -84,6 +95,12 @@ public class UserServiceTest {
 		checkLevelUpgraded(users.get(2), false);
 		checkLevelUpgraded(users.get(3), true);
 		checkLevelUpgraded(users.get(4), false);
+		
+		// 목 오브젝트에 저장된 메일 수신자 목록을 가져와 업그레이드 대상과 일치하는지 확인 한다.
+		List<String> request = mockMailSender.getRequests();
+		assertThat(request.size(), is(2));
+		assertThat(request.get(0), is(users.get(1).getEmail()));
+		assertThat(request.get(1), is(users.get(3).getEmail()));
 	}
 	
 	/**
@@ -155,4 +172,32 @@ public class UserServiceTest {
 	public static void main(String[] args) {
 		JUnitCore.main("springbook.user.service.UserServiceTest");
 	}
+	
+	//---------------------------------------------------------------------------------------------------------
+	// 스태틱 클래스
+	//---------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * 리스트 5-57 목 오브젝트로 만든 메일 전송 확인용 클래스
+	 */
+	static class MockMailSender implements MailSender {
+		
+		// UserService로부터 전송 요청을 받은 메일 주소를 저장해두고 이를 읽을 수 있게 한다.
+		private List<String> requests = new ArrayList<String>();
+		public List<String> getRequests() {
+			return requests;
+		}
+		
+		@Override
+		public void send(SimpleMailMessage simpleMessage) throws MailException {
+			// TODO Auto-generated method stub
+			requests.add(simpleMessage.getTo()[0]);				// 전송 요청을 받은 이메일 주소를 저장해둔다. 간단하게 첫 번째 수신자 메일 주소만 저장했다.
+		}
+		@Override
+		public void send(SimpleMailMessage... simpleMessages) throws MailException {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
 }
