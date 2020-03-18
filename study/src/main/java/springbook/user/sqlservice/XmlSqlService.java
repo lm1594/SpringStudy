32f680b1f6a578ef9d3a24932b8a8d4291577e78
이경민 <@DESKTOP-B8C9N61>
2020-03-18 18:48:43 +0900

    7.2.2 XML 파일을 이용하는 SQL 서비스

---
+++ study/src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -0,0 +1,56 @@
+package springbook.user.sqlservice;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import springbook.user.dao.UserDao;
+import springbook.user.sqlservice.jaxb.SqlType;
+import springbook.user.sqlservice.jaxb.Sqlmap;
+
+/**
+ * 토비의 스프링
+ * @author 이경민
+ * @since 2020.03.18
+ * @history
+ * 7장 스프링 핵심기술의 응용
+ *   7.2장 인터페이스의 분리와 자기참조 빈
+ *    - 7.2.2 XML파일을 이용하는 SQL 서비스
+ */
+public class XmlSqlService implements SqlService {
+	// 설정파일에 <map>으로 정의된 SQL 정보를 가져오도록 프로퍼티로 등록해둔다.
+	private Map<String, String> sqlMap = new HashMap<String, String>();				// 읽어온 SQL을 저장해둘 맵
+	
+	public XmlSqlService() {														// 스프링이 오브젝트를 만드는 시점에서 SQL을 읽어오도록 생성자를 이용한다.
+		// UserDao와 같은 클래스패스의 sqlmap.xml 파일을 변환한다.
+		String contextPath = Sqlmap.class.getPackage().getName();
+		try {
+			JAXBContext context = JAXBContext.newInstance(contextPath);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+			Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(is);
+			for(SqlType sql : sqlmap.getSql()) {
+				sqlMap.put(sql.getKey(), sql.getValue());							// 읽어온 SQL을 맵으로 저장해둔다.
+			}
+		}catch (JAXBException e) {
+			throw new RuntimeException(e);											// JAXBException은 복구 불가능한 예외다. 불필요한 throws를 피하도록 런타임 예외로 포장해서 던진다.
+		}
+		
+		
+	}
+	
+	@Override
+	public String getSql(String key) throws SqlRetrievalFailureException {
+		String sql = sqlMap.get(key);
+		if(sql == null) {
+			throw new SqlRetrievalFailureException(key + "에 대한 SQL을 찾을 수 없습니다.");
+		}else {
+			return sql;
+		}
+	}
+
+}
